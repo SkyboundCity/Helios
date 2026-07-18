572788aa3ac64aefb1bea0908a6b5eb302303010
TehBrian <tehbrian@proton.me>
2026-07-17 21:11:05 -0400

    improve game mode commands
    - only send message if game mode would change
    - add aliases
    - add spectator command

--- src/main/java/city/skybound/helios/server/GameModeCommands.java
+++ src/main/java/city/skybound/helios/server/GameModeCommands.java
@@ -5,6 +5,7 @@ import city.skybound.helios.config.LangConfig;
 import com.google.inject.Inject;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.GameMode;
+import org.incendo.cloud.context.CommandContext;
 import org.incendo.cloud.paper.PaperCommandManager;
 import org.incendo.cloud.paper.util.sender.PlayerSource;
 import org.incendo.cloud.paper.util.sender.Source;
@@ -23,46 +24,54 @@ public final class GameModeCommands {
 		this.langConfig = langConfig;
 	}
 
-	public void register(final PaperCommandManager<Source> commandManager) {
-		final var main = commandManager.commandBuilder("gamemode", "gm")
+	public void register(final PaperCommandManager<Source> cm) {
+		final var main = cm.commandBuilder("gamemode", "gm")
 				.permission(Permission.GAMEMODE)
 				.senderType(PlayerSource.class)
 				.commandDescription(description("Change your game mode."));
 
-		final var survival = main.literal("survival", "s")
-				.handler(c -> {
-					final var sender = c.sender().source();
-					sender.setGameMode(GameMode.SURVIVAL);
-					sender.sendMessage(this.langConfig.c(
-							NodePath.path("gamemode", "change"),
-							Placeholder.unparsed("gamemode", "Survival")
-					));
-				});
+		final var cSurvival = main.literal("survival").handler(this::executeSurvival);
+		final var cCreative = main.literal("creative").handler(this::executeCreative);
+		final var cAdventure = main.literal("adventure").handler(this::executeAdventure);
+		final var cSpectator = main.literal("spectator").handler(this::executeSpectator);
 
-		final var creative = main.literal("creative", "c")
-				.handler(c -> {
-					final var sender = c.sender().source();
-					sender.setGameMode(GameMode.CREATIVE);
-					sender.sendMessage(this.langConfig.c(
-							NodePath.path("gamemode", "change"),
-							Placeholder.unparsed("gamemode", "Creative")
-					));
-				});
+		cm.command(cSurvival);
+		cm.command(cCreative);
+		cm.command(cAdventure);
+		cm.command(cSpectator);
 
-		final var adventure = main.literal("adventure", "a")
-				.handler(c -> {
-					final var sender = c.sender().source();
-					sender.setGameMode(GameMode.ADVENTURE);
-					sender.sendMessage(this.langConfig.c(
-							NodePath.path("gamemode", "change"),
-							Placeholder.unparsed("gamemode", "Adventure")
-					));
-				});
-
-		commandManager.command(main);
-		commandManager.command(survival);
-		commandManager.command(creative);
-		commandManager.command(adventure);
+		cm.command(cm.commandBuilder("gms").proxies(cSurvival.build()));
+		cm.command(cm.commandBuilder("gmc").proxies(cCreative.build()));
+		cm.command(cm.commandBuilder("gma").proxies(cAdventure.build()));
 	}
 
+
+	private void executeSurvival(final CommandContext<PlayerSource> c) {
+		execute(c, GameMode.SURVIVAL, "Survival");
+	}
+
+	private void executeCreative(final CommandContext<PlayerSource> c) {
+		execute(c, GameMode.CREATIVE, "Creative");
+	}
+
+	private void executeAdventure(final CommandContext<PlayerSource> c) {
+		execute(c, GameMode.ADVENTURE, "Adventure");
+	}
+
+	private void executeSpectator(final CommandContext<PlayerSource> c) {
+		execute(c, GameMode.SPECTATOR, "Spectator");
+	}
+
+	private void execute(final CommandContext<PlayerSource> c, final GameMode gameMode, final String gameModeString) {
+		final var sender = c.sender().source();
+		if (sender.getGameMode().equals(gameMode)) {
+			return;
+		}
+
+		sender.setGameMode(gameMode);
+		sender.sendMessage(this.langConfig.c(
+				NodePath.path("gamemode", "change"),
+				Placeholder.unparsed("gamemode", gameModeString)
+		));
+	}
 }
